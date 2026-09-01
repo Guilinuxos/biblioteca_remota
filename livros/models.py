36331e3ff403cc8aa9d4b28f469abd3836ec7d1c
Guilherme Barros <guilhermecassimiro425@gmail.com>
2026-08-31 21:07:28 -0300

    Criacao da classe Livro

---
+++ livros/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+# Create your models here.
+class Livro(models.Model):
+    titulo = models.CharField(max_length = 200)
+    autor = models.CharField(max_length = 200)
+    editora = models.CharField(max_length = 200)
+    ano = models.PositiveIntegerField(
+        validators = [MinValueValidator(1000), MaxValueValidator(9999)]
+    )
+
